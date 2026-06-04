67994f1bc8b7350106c03aaa0fbcf49f9dfdbf45
zxc-byte <mdollar800@gmail.com>
2026-06-05 00:32:17 +0900

    all done

--- 2nd year(4st semester)/19. Otzig/src/Annealing.java
+++ 2nd year(4st semester)/19. Otzig/src/Annealing.java
@@ -7,14 +7,14 @@ public class Annealing {
         double v2 = 0.4 * Math.pow(x[1] - 0.5, 2);
         double v3 = 0.8 * Math.pow(x[2] - 0.3, 2);
         return Math.sqrt(v1 + v2 + v3 + 8);
-    }
+    } b
 
     public static void main(String[] args) {
 
         Random random = new Random();
 
         // Начальная точка (3 координаты)
-        double[] xOld = {-1.3, 2.7, 0.0};
+        double[] xOld = {0.0, 0.0, 0.0};
 
         double[] xNew = new double[3];
 
@@ -26,7 +26,7 @@ public class Annealing {
 
         // Заголовок таблицы результатов
         // вероястность, что алгоритм согласится на ухудшение функции
-        System.out.println("k\tt\t     x1\t     x2\t     x3\t     f(x)\t   df\tвероятность\tдействие");
+        System.out.println("k\t  t\t     x1\t     x2\t     x3\t     f(x)\t   df\tвероятность\tдействие");
         System.out.printf("%d\t%.4f\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f\t%.3f\t%s%n",
                 k, t, xOld[0], xOld[1], xOld[2], f(xOld), 0.0, 0.0, "старт");
 
